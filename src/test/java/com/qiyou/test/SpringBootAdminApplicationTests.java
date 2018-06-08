15182df6c117e1037737758002ce23ff62caea62
wangzhipeng1992 <zhipengwang1992@163.com>
2018-06-08 17:11:19 +0800

    test

--- src/test/java/com/qiyou/test/SpringBootAdminApplicationTests.java
+++ src/test/java/com/qiyou/test/SpringBootAdminApplicationTests.java
@@ -2,6 +2,7 @@ package com.qiyou.test;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,11 +14,13 @@ import org.springframework.test.context.web.WebAppConfiguration;
 public class SpringBootAdminApplicationTests {
 
     @Before
+    @Test
     public void init() {
         System.out.println("开始测试-----------------");
     }
 
     @After
+    @Test
     public void after() {
         System.out.println("测试结束-----------------");
     }
