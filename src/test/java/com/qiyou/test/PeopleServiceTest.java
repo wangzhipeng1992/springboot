eec52fcf1425c2c72012bfb02663294a86d725ec
wangzhipeng1992 <zhipengwang1992@163.com>
2018-06-12 17:38:15 +0800

    Test To Update

--- src/test/java/com/qiyou/test/PeopleServiceTest.java
+++ src/test/java/com/qiyou/test/PeopleServiceTest.java
@@ -7,19 +7,25 @@ import com.qiyou.basic.Result;
 import com.qiyou.persistence.service.PeopleService;
 import com.qiyou.util.JsonHelper;
 
-public abstract class PeopleServiceTest extends SpringBootAdminApplicationTests {
+/**
+ * @description : 1. class can't make to abstract
+ *                2. method only at here only for simple test, can't to jenkins 
+ * @author Lenovo
+ *
+ */
+public class PeopleServiceTest extends SpringBootAdminApplicationTests {
 
     @Autowired
     private PeopleService peopleService;
 
     @Test
     public void getPeopleInfo() {
-        /*try {
+        try {
             Result pResult = peopleService.getPeopleInfoFromCache();
             System.out.println(JsonHelper.parseToJson(pResult));
         } catch (Exception e) {
             e.printStackTrace();
-        }*/
+        }
         System.out.println("-----------------由于虚拟机未安装相应数据库,这里仅输出代表数据返回-----------------");
     }
 
--- src/test/java/com/qiyou/test/SpringBootAdminApplicationTests.java
+++ src/test/java/com/qiyou/test/SpringBootAdminApplicationTests.java
@@ -4,25 +4,45 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.qiyou.persistence.service.PeopleService;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @WebAppConfiguration
+/**
+ * @description 1.package as far as possible to com.qiyou, if this maby need springBootTest(class = SpringBootAdminApplication.class)
+ *              2.if @Before & @After add @Test, method init() and after() will run three times
+ * @author Lenovo
+ *
+ */
 public class SpringBootAdminApplicationTests {
 
     @Before
-    @Test
     public void init() {
         System.out.println("开始测试-----------------");
     }
 
     @After
-    @Test
     public void after() {
         System.out.println("测试结束-----------------");
+    }
+
+    @Autowired
+    private PeopleService peopleService;
+
+    @Test
+    public void getPeopleInfo() {
+        /*
+         * try { Result pResult = peopleService.getPeopleInfoFromCache();
+         * System.out.println(JsonHelper.parseToJson(pResult)); } catch
+         * (Exception e) { e.printStackTrace(); }
+         */
+        System.out.println("-----------------由于虚拟机未安装相应数据库,这里仅输出代表数据返回-----------------");
     }
 
 }
