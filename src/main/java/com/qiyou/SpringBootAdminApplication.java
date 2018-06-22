26bb82b14f1d382e929e260a89bb700e62fa4bad
wangzhipeng1992 <zhipengwang1992@163.com>
2018-06-22 13:36:40 +0800

    rm syso add log

--- src/main/java/com/qiyou/SpringBootAdminApplication.java
+++ src/main/java/com/qiyou/SpringBootAdminApplication.java
@@ -10,7 +10,6 @@ import org.springframework.cache.annotation.EnableCaching;
 public class SpringBootAdminApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-	    System.out.println("fuck");
 		SpringApplication.run(SpringBootAdminApplication.class, args);
 	}
 
